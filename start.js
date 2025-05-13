44f87cf5098b89adf342ed5694aaac2b23b9fa29
6Morpheus6 <wanton-decay@gmx.net>
2025-05-13 23:58:58 +0200

    remove proxy

--- start.js
+++ start.js
@@ -1,7 +1,7 @@
-module.exports = async (kernel) => {
-  let script = {
-    daemon: true,
-    run: [{
+module.exports = {
+  daemon: true,
+  run: [
+    {
       method: "shell.run",
       params: {
         venv: "env",
@@ -9,20 +9,17 @@ module.exports = async (kernel) => {
         message: [
           "python app.py",
         ],
-        on: [{ "event": "/http:\/\/[0-9.:]+/", "done": true }]
+        on: [{
+          event: "/http:\/\/[0-9.:]+/",
+          done: true
+        }]
       }
-    }, {
-      "method": "local.set",
-      "params": {
-        "url": "{{input.event[0]}}"
+    },
+    {
+      method: "local.set",
+      params: {
+        url: "{{input.event[0]}}"
       }
-    }, {
-      "method": "proxy.start",
-      "params": {
-        "uri": "{{local.url}}",
-        "name": "Local Sharing"
-      }
-    }]
-  }
-  return script
+    }
+  ]
 }
